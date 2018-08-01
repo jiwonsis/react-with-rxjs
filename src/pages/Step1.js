a7876352f9e92757c306451e1af8efaac902a826
Scott Moon <scott@dogmate.co.kr>
2018-08-02 01:51:55 +0900

    step1 자동완성 UI 중간 개발

--- src/pages/Step1.js
+++ src/pages/Step1.js
@@ -6,40 +6,83 @@ import {
 	debounceTime,
 	filter,
 	distinctUntilChanged,
+	tap,
+	partition,
 } from 'rxjs/operators'
 import {ajax} from 'rxjs/ajax'
 import './Step1.css';
 
-const searchList = (user) => (
+const searchList = (user = {avatar_url: '', html_url: '', login: ''}) => (
 	<li className="user" key={user.id}>
-		<img src={user.avatar_url} width="50px" height="50px" />
+		<img src={user.avatar_url} width="50px" height="50px" alt="프로필"/>
 		<p><a href={user.html_url} target="_blank">{user.login}</a></p>
 	</li>
 );
 
 class AutoCompletedUI extends Component {
-	keyUp$ = null;
+	$loading = null;
 	
 	constructor() {
 		super();
-		this.state = {items: []}
+		this.state = {
+			items: [],
+			loading: false,
+		}
 	}
 	
 	componentDidMount() {
-		const user$ = fromEvent(document.getElementById('search'), 'keyup')
+		this.$loading = document.getElementById('loading');
+		const keyUp$ = fromEvent(document.getElementById('search'), 'keyup')
 			.pipe(
 				debounceTime(300),
 				map(event => event.target.value),
 				distinctUntilChanged(),
-				filter(query => query.trim().length > 0),
-				mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
 			);
 		
-		user$.subscribe(data => {
-			console.log(data);
-			this.setState({items: data.items});
-		});
+		let [user$, reset$] = keyUp$
+			.pipe(
+				partition(query => query.trim().length > 0)
+			);
 		
+		user$ = keyUp$
+			.pipe(
+				tap(_ => this.showLoading()),
+				mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
+				tap(_ => this.hideLoading()),
+			);
+		
+		reset$ = keyUp$
+			.pipe(
+				tap(() => this.resetState()),
+			).subscribe();
+		
+		user$.subscribe(data => {
+			this.setState({
+				...this.state,
+				items: data.items
+			});
+		});
+	}
+	
+	resetState() {
+		this.setState({
+			items: [],
+			loading: false,
+		})
+	}
+	
+	showLoading() {
+		this.setState({
+			...this.state,
+			loading: true,
+		});
+	}
+	
+	hideLoading() {
+		this.setState({
+			...this.state,
+			loading: false,
+		});
 	}
 	
 	render() {
@@ -59,6 +102,13 @@ class AutoCompletedUI extends Component {
 							)
 						}
 					</ul>
+					{
+						this.state.loading && (
+							<div id="loading" >
+								<i className="fas fa-spinner fa-pulse"/>
+							</div>
+						)
+					}
 				</div>
 			</Fragment>
 		)
