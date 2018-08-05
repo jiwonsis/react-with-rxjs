dd110455ae929d74cc2040e2d32e05f9d54b2a30
Scott Moon <scott@dogmate.co.kr>
2018-08-05 14:20:16 +0900

    step1 자동완성 UI 개발

--- src/pages/Step1.js
+++ src/pages/Step1.js
@@ -2,11 +2,12 @@ import React, {Component, Fragment} from 'react';
 import {fromEvent} from 'rxjs';
 import {
 	map,
-	mergeMap,
+	switchMap,
 	debounceTime,
-	filter,
 	distinctUntilChanged,
+	finalize,
 	tap,
+	retry,
 	partition,
 } from 'rxjs/operators'
 import {ajax} from 'rxjs/ajax'
@@ -20,7 +21,6 @@ const searchList = (user = {avatar_url: '', html_url: '', login: ''}) => (
 );
 
 class AutoCompletedUI extends Component {
-	$loading = null;
 	
 	constructor() {
 		super();
@@ -34,21 +34,23 @@ class AutoCompletedUI extends Component {
 		this.$loading = document.getElementById('loading');
 		const keyUp$ = fromEvent(document.getElementById('search'), 'keyup')
 			.pipe(
-				debounceTime(300),
-				map(event => event.target.value),
-				distinctUntilChanged(),
+				debounceTime(300),  // 300ms 뒤에 데이터 전달
+				map(event => event.target.value),  // 키 이벤트 중에, target.value 만 이벤트 값만 데이터 전달
+				distinctUntilChanged(),     // 특수키가 입력된 경우 이벤트 호출 방지 (value 값이 동일한 경우는 데이터 전달 중지)
 			);
 		
-		let [user$, reset$] = keyUp$
+		let {user$, reset$} = keyUp$
 			.pipe(
-				partition(query => query.trim().length > 0)
+				partition(query => query.trim().length > 0)  // 파티션 함수의 참이 되는 Observable 은 user$, 거짓인경우, reset$ 으로 두개의 Observable 로 분리
 			);
 		
 		user$ = keyUp$
 			.pipe(
-				tap(_ => this.showLoading()),
-				mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
-				tap(_ => this.hideLoading()),
+				tap(() => this.showLoading()),  // rxjs5 에서는 do라는 함수를 써서 해당 이벤트를 발생시켰으나, 현재는 tap 으로 처리
+				switchMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)), // 기존 처리가 완료 안된 상태에서 새로운 처리가 들어온 경우 기존 데이터는 unsubscribe 처리하고 새로운 데이터를 처리함
+				tap(() => this.hideLoading()),
+				retry(2), // throw error 가 발생했을 경우, 2번까지는 제시도 가능
+				finalize(() => this.hideLoading()),     // try-catch 의 finally 와 동일한 이벤트 발생
 			);
 		
 		reset$ = keyUp$
@@ -56,11 +58,17 @@ class AutoCompletedUI extends Component {
 				tap(() => this.resetState()),
 			).subscribe();
 		
-		user$.subscribe(data => {
-			this.setState({
-				...this.state,
-				items: data.items
-			});
+		user$.subscribe({
+			next: (data) => {       // 구독된 데이터가 정상일 경우의 처리
+				this.setState({
+					...this.state,
+					items: data.items
+				});
+			},
+			error: (error) => {     // 구독된 데이터에서 에러가 발생했을 경우, 해당 이벤트 발생
+				console.error(error);
+				alert(error.message);
+			},
 		});
 	}
 	
