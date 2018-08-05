73fd07142c7ee6d3a23fa7e4e7402885d8f92cf3
Scott Moon <scott@dogmate.co.kr>
2018-08-05 14:33:09 +0900

    step1 partition 오류 수정

--- src/pages/Step1.js
+++ src/pages/Step1.js
@@ -37,25 +37,31 @@ class AutoCompletedUI extends Component {
 				debounceTime(300),  // 300ms 뒤에 데이터 전달
 				map(event => event.target.value),  // 키 이벤트 중에, target.value 만 이벤트 값만 데이터 전달
 				distinctUntilChanged(),     // 특수키가 입력된 경우 이벤트 호출 방지 (value 값이 동일한 경우는 데이터 전달 중지)
+				tap(v => console.log('from keyup$', v)),
 			);
 		
-		let {user$, reset$} = keyUp$
+		let [user$, reset$] = keyUp$
 			.pipe(
-				partition(query => query.trim().length > 0)  // 파티션 함수의 참이 되는 Observable 은 user$, 거짓인경우, reset$ 으로 두개의 Observable 로 분리
+				partition(query => {
+					console.log('part ', query.trim().length > 0);
+					return query.trim().length > 0;
+				})  // 파티션 함수의 참이 되는 Observable 은 user$, 거짓인경우, reset$ 으로 두개의 Observable 로 분리
 			);
 		
-		user$ = keyUp$
+		user$ = user$
 			.pipe(
 				tap(() => this.showLoading()),  // rxjs5 에서는 do라는 함수를 써서 해당 이벤트를 발생시켰으나, 현재는 tap 으로 처리
 				switchMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)), // 기존 처리가 완료 안된 상태에서 새로운 처리가 들어온 경우 기존 데이터는 unsubscribe 처리하고 새로운 데이터를 처리함
 				tap(() => this.hideLoading()),
 				retry(2), // throw error 가 발생했을 경우, 2번까지는 제시도 가능
 				finalize(() => this.hideLoading()),     // try-catch 의 finally 와 동일한 이벤트 발생
+				tap(v => console.log('from user$', v)),
 			);
 		
-		reset$ = keyUp$
+		reset$
 			.pipe(
 				tap(() => this.resetState()),
+				tap(v => console.log('from reset$', v)),
 			).subscribe();
 		
 		user$.subscribe({
